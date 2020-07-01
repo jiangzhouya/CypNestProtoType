6eb3c90d068dc1461c8536ac3ab9aa67d8d5c245
Zhouya JIANG <32921795+jiangzhouya@users.noreply.github.com>
2020-07-01 16:18:24 +0800

    Report类增加2个属性

--- wdReports/Model/Report.cs
+++ wdReports/Model/Report.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using wdReports.Annotations;
 
 namespace wdReports
 {
-    class Report
+    public class Report : INotifyPropertyChanged
     {
+        private string reportName;
+        private string description;
+
+        public string ReportName
+        {
+            get => reportName;
+            set
+            {
+                reportName = value;
+                OnPropertyChanged(nameof(ReportName));
+            }
+        }
+        public string Description
+        {
+            get => description;
+            set
+            {
+                description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
