9f67528c712be8694fbaf606508973c88e596736
Zhouya JIANG <32921795+jiangzhouya@users.noreply.github.com>
2020-07-01 20:21:20 +0800

    创建模型类的属性，开始写VM

--- wdReports/Model/Task.cs
+++ wdReports/Model/Task.cs
@@ -1,14 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using wdReports.Annotations;
 
 namespace wdReports
 {
-    public class Task
+    public class Task : INotifyPropertyChanged
     {
         private string taskName;
+        private string material;
+        private string thickness;
+        private string cuttingGas;
+        private string customerName;
 
+
+        public string TaskName
+        {
+            get => taskName;
+            set
+            {
+                taskName = value;
+                OnPropertyChanged(nameof(TaskName));
+            }
+        }
+        public string Material
+        {
+            get => material;
+            set
+            {
+                material = value;
+                OnPropertyChanged(nameof(Material));
+            }
+        }
+        public string Thickness
+        {
+            get => thickness;
+            set
+            {
+                thickness = value;
+                OnPropertyChanged(nameof(Thickness));
+            }
+        }
+        public string CuttingGas
+        {
+            get => cuttingGas;
+            set
+            {
+                cuttingGas = value;
+                OnPropertyChanged(nameof(CuttingGas));
+            }
+        }
+        public string CustomerName
+        {
+            get => customerName;
+            set
+            {
+                customerName = value;
+                OnPropertyChanged(nameof(CustomerName));
+            }
+        }
+
+        public Task()
+        {
+            taskName = "taskName";
+            this.material = "碳钢";
+            this.thickness = "1mm";
+            this.cuttingGas = "空气";
+            this.customerName = "CustomerX";
+        }
+
+        public Task(string taskName, string material, string thickness, string cuttingGas, string customerName)
+        {
+            this.taskName = taskName;
+            this.material = material;
+            this.thickness = thickness;
+            this.cuttingGas = cuttingGas;
+            this.customerName = customerName;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- wdReports/ViewModel/ViewModel.cs
+++ wdReports/ViewModel/ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,7 @@ namespace wdReports.ViewModel
 {
     public class ViewModel
     {
+        public ObservableCollection<Report> reportsList = new ObservableCollection<Report>();
+
     }
 }
